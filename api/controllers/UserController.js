d4c9212b6b4ddf16e34a21577838d32fce66628d
xiq xiao <xiqxiq@rocketmail.com>
2015-06-22 23:05:53 +0200

    initial commit

---
+++ api/controllers/UserController.js
@@ -0,0 +1,82 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+  /**
+   * `UserController.new()`
+   */
+  new: function (req, res) {
+
+    console.log("hahaha");
+
+    return res.json({
+      todo: 'new() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.destroy()`
+   */
+  destroy: function (req, res) {
+    return res.json({
+      todo: 'destroy() is not implemented yet!'
+    });
+  }
+};
+
---
+++ api/controllers/WelcomeController.js
@@ -0,0 +1,21 @@
+/**
+ * WelcomeController
+ *
+ * @description :: Server-side logic for managing welcomes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `WelcomeController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  }
+};
+
